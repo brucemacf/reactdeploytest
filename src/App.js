255cea71733c276a3c7465fb6f5ac6bb42de5ad6
Bruce MacFarlane <brucemacfarlane@Bruces-MacBook-Pro.local>
2018-10-21 14:26:06 -0700

    hooking up test call to Lamda

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputText: '',
+    isLoading: false,
+    errorMessage: null,
+    response: null
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if(!this.state.inputText) { return; }
+    console.log('handleSubmit');
+    this.setState({isLoading: true, response: null, errorMessage: null});
+    axios.post(
+      'https://f2i2maamrj.execute-api.us-east-1.amazonaws.com/default/ReactTestS3DeployEndpoint',
+      {
+        name: this.state.inputText
+      },
+      {
+        headers: {'Content-Type': 'application/json'}
+      }
+    )
+      .then((response) => {
+        this.setState({isLoading: false, response: response.data.message});
+        // console.log('-->', response);
+      })
+      .catch((error) => {
+        this.setState({isLoading: false, errorMessage: error.message});
+        // console.log('err:', error.message);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          {/* <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          </p> */}
+          <form onSubmit={this.handleSubmit} style={{zIndex: '1'}}>
+            <input type="text" className="name" placeholder="Enter name" onChange={(event) => this.setState({inputText: event.target.value}) } value={this.state.inputText} />
+            {this.state.isLoading ? 'Loading...' : <input type="submit" value="Submit" />}
+            <div><i style={{color: "red", fontSize: "12px"}}>{this.state.errorMessage}</i></div>
+            <hr />
+            Message:
+            <div className="msg">{this.state.response}</div>
+          </form>
+
         </header>
       </div>
     );
